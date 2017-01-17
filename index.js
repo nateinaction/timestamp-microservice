570e4f68bf8069c567529dc0af5a555387755449
Nate Gay <nate@ohnate.com>
2017-01-17 11:57:51 -0600

    working timestamp microservice

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express')
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var errorObj = {unix: null, natural: null, error: 'Please include Unix date or string formatted as a valid date and time'}
+
+var dateResponse = (res, data) => {
+  if (isNaN(data)) {
+    // Formatted date
+    var natural = decodeURIComponent(data)
+    var date = new Date(natural)
+    var unix = date.getTime()
+    if (isNaN(unix)) {
+      // date is not valid
+      return res.json(errorObj)
+    }
+  } else {
+    // Unix date
+    var unix = parseInt(data)
+    var date = new Date(unix)
+    var natural = months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
+  }
+  var obj = {
+    unix,
+    natural
+  }
+  return res.json(obj)
+}
+
+var app = express()
+app.get('/:data', (req, res) => {
+  var data = req.params.data || null
+  if (data) {
+    dateResponse(res, data)
+  } else {
+    return res.json(errorObj)
+  }
+})
+
+app.listen(process.env.PORT || 5000)
